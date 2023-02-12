83d02368f8aa4bf34bcb02a65f4cf1e06de1e371
WalastyanDA <dwalastyan@gmail.com>
2023-02-12 10:59:02 +0000

    Working Test2

--- ghost-hunt/templates/script.js
+++ ghost-hunt/templates/script.js
@@ -1,6 +1,32 @@
 const output = document.querySelector('#terminal-output');
 const commandLine = document.querySelector('#command-line');
 
+function typeASCIIArt() {
+  const art = `
+
+
+
+  /$$$$$$  /$$   /$$  /$$$$$$   /$$$$$$  /$$$$$$$$       /$$   /$$ /$$   /$$ /$$   /$$ /$$$$$$$$
+ /$$__  $$| $$  | $$ /$$__  $$ /$$__  $$|__  $$__/      | $$  | $$| $$  | $$| $$$ | $$|__  $$__/
+| $$  |__/| $$  | $$| $$  | $$| $$  |__/   | $$         | $$  | $$| $$  | $$| $$$$| $$   | $$   
+| $$ /$$$$| $$$$$$$$| $$  | $$|  $$$$$$    | $$         | $$$$$$$$| $$  | $$| $$ $$ $$   | $$   
+| $$|_  $$| $$__  $$| $$  | $$ |____  $$   | $$         | $$__  $$| $$  | $$| $$  $$$$   | $$   
+| $$  | $$| $$  | $$| $$  | $$ /$$  | $$   | $$         | $$  | $$| $$  | $$| $$|  $$$   | $$   
+|  $$$$$$/| $$  | $$|  $$$$$$/|  $$$$$$/   | $$         | $$  | $$|  $$$$$$/| $$ |  $$   | $$   
+ |______/ |__/  |__/ |______/  |______/    |__/         |__/  |__/ |______/ |__/  |__/   |__/   
+  `;
+  
+  let i = 0;
+  const type = setInterval(() => {
+    output.innerHTML = art.substring(0, i);
+    i++;
+    if (i > art.length) {
+      clearInterval(type);
+    }
+  }, 3);
+}
+
+typeASCIIArt();
 commandLine.addEventListener('keydown', (event) => {
   if (event.key === 'Enter') {
     const command = commandLine.value;
@@ -9,14 +35,14 @@ commandLine.addEventListener('keydown', (event) => {
     output.appendChild(line);
     commandLine.value = '';
     
-    while (output.children.length > 10) {
+    while (output.children.length > 23) {
       output.removeChild(output.firstChild);
     }
     
     const lines = output.querySelectorAll('div');
     for (const line of lines) {
-      if (line.innerHTML.length > 80) {
-        line.innerHTML = line.innerHTML.substr(0, 80) + '...';
+      if (line.innerHTML.length > 120) {
+        line.innerHTML = line.innerHTML.substr(0, 120) + '...';
       }
     }
   }
